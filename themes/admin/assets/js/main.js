b7cd0caaa8c2884a76d595315d074f3c8d3a5150
Fayaz K <allshore.php44@gmail.com>
2017-12-31 01:22:53 +0500

    Adjusted JS

--- themes/admin/assets/js/main.js
+++ themes/admin/assets/js/main.js
@@ -1,53 +1,59 @@
 var SalFay;
-jQuery(function ($) {
-    SalFay = {
+jQuery(function($) {
+  SalFay = {
 
-        /**
-         * jQuery Body Object
-         */
-        $body: $('body'),
+    /**
+     * jQuery Body Object
+     */
+    $body: $('body'),
 
-        /**
-         * jQuery Window Object
-         */
-        $window: $(window),
+    /**
+     * jQuery Window Object
+     */
+    $window: $(window),
 
-        /**
-         *  jQuery Document Object
-         */
-        $document: $(document),
+    /**
+     *  jQuery Document Object
+     */
+    $document: $(document),
 
-        /**
-         *
-         */
-        confirmAction: function () {
-            this.$body.on('click', '[data-action="confirm"]', function (e) {
-                e.preventDefault();
-                var href = $(this).attr('href');
-                alertify.confirm('Delete', 'Do you really want to delete this?', function () {
-                    window.location.href = href;
-                }, function () {
+    /**
+     *
+     */
+    initEvents: function() {
+      this.$body.
+          on('click', '[data-action="confirm"]', function(e) {
+            e.preventDefault();
+            var href = $(this).attr('href');
+            alertify.confirm('Delete', 'Do you really want to delete this?',
+                function() {
+                  window.location.href = href;
+                }, function() {
                 });
+          }).
+          on('click', '[data-show-listing]', function() {
+            $('#edit-module').slideUp();
+            $('#listing-module').slideDown();
+          }).
+          on('click', '[data-show-edit]', function() {
+            $('#listing-module').slideUp();
+            $('#edit-module').slideDown();
+          });
+    },
 
-            });
-        },
+    /**
+     * Show/Hide Divs
+     * @param showDive
+     * @param hideDiv
+     */
+    toggleDivs: function(showDive, hideDiv) {
+      $(showDive).slideDown();
+      $(hideDiv).slideUp();
+    },
 
-        /**
-         * Show/Hide Divs
-         * @param showDive
-         * @param hideDiv
-         */
-        toggleDivs: function (showDive, hideDiv) {
-            $(showDive).slideDown();
-            $(hideDiv).slideUp();
-        },
-
-
-        init: function () {
-            $(document).ready(function () {
-                SalFay.confirmAction();
-            });
-        }
-    };
-    SalFay.init();
+    init: function() {
+      this.initEvents();
+    },
+  };
+  SalFay.init();
 });
